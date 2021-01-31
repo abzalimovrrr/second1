878175c03c2f09c380e034dd26544d66667409e3
Rishat <abzalimovrrr@yandex.ru>
2021-01-31 17:06:21 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log(2);
+console.log(1);
+let namber=5;
+let namber1=9;
+let namber2=7;
+let namber3=8;
+
+console.log(namber);
+namber=10;
+console.log(namber);
+namber=namber+namber1-namber2;
+console.log(namber);
+let obj = {
+    a:"cvbncvbn",
+    b:"hjkghjk",
+    c:"cvbn"
+}; 
+console.log(obj.b);
+let arr = ['aa','ss','dd','ffff','gggg','hhhh','jjj',{aaa:"cgdfg",b:"dfgdfg"}]; 
+console.log(arr[7]);
+const asa = prompt("ВАСЯ","dfgsdfgsdfgsdfgsdfgsdfg");
+console.log(asa);
+const answe = prompt("ВАСЯ","dfgsdfgsdfgsdfgsdfgsdfg");
+const answer=[];
+answer[0]=prompt("Имя","");
+answer[1]=prompt("Фамилия","");
+answer[2]=prompt("Отчество","");
+console.log(answer[1]);
+console.log(answer[0]);
+console.log(answer[2]);
+document.write(answer);
+/* Document.write(answer[0]);
+Document.write(answer[2]);
+console.log(answer); */
+console.log(answer[2]+`Петя Вася${obj.b}DFDF`);
